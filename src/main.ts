8f9d7ce6b70509ca61dd98618730a414e4b069ad
Vue <audrb0999@naver.com>
2024-02-16 17:56:33 +0900

    메인화면 수정

--- src/main.ts
+++ src/main.ts
@@ -7,33 +7,31 @@ import { select } from './utils/ElementUtils';
 export class Main {
   render(): string {
     return `
-    <section class="bg-gray-50">
+      <section class="bg-gray-900 text-white">
         <div class="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
-        <div class="mx-auto max-w-xl text-center">
-            <h1 class="text-3xl font-extrabold sm:text-5xl">
-            Music Comparison Site
-            <strong class="font-extrabold text-red-700 sm:block"> Increase Conversion. </strong>
+          <div class="mx-auto max-w-3xl text-center">
+            <h1 class="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl">
+              Music Comparison Site
+              <span class="sm:block"> Increase Conversion. </span>
             </h1>
-    
-            <p class="mt-4 sm:text-xl/relaxed">
-            Search for the music you want on various music sites.<br>
-            Melon, Genie, FLO, Vibe, Bugs 
-                       
+            <p class="mx-auto mt-4 max-w-xl sm:text-xl/relaxed">
+              Search for the music you want on various music sites.<br>
+              Melon, Genie, FLO, Vibe, Bugs 
             </p>
-    
+      
             <div class="mt-8 flex flex-wrap justify-center gap-4">
-            <button id="getStart" class="block w-full rounded bg-red-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-red-700 focus:outline-none focus:ring active:bg-red-500 sm:w-auto">
+              <button id="getStart" class="block w-full rounded border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-white focus:outline-none focus:ring active:text-opacity-75 sm:w-auto" href="#">
                 Get Started
-            </button>
-    
-            <button style="display:none;" class="block w-full rounded px-12 py-3 text-sm font-medium text-red-600 shadow hover:text-red-700 focus:outline-none focus:ring active:text-red-500 sm:w-auto">
+              </button>
+      
+              <button style="display:none;" class="block w-full rounded border border-blue-600 px-12 py-3 text-sm font-medium text-white hover:bg-blue-600 focus:outline-none focus:ring active:bg-blue-500 sm:w-auto" href="#">
                 Learn More
-            </button>
+              </button>
             </div>
+          </div>
         </div>
-        </div>
-    </section>
-        `;
+      </section>
+    `;
   }
   getStart() {
     const getStart = select('#getStart');
